5ebc155c242dac9fa13b5c47907dba46ded1ac9c
tommy <tommypeled10@gmail.com>
2025-06-20 17:23:39 +0200

    fixed death bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -80,6 +80,8 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        _collider = GetComponent<Collider2D>();
+
         _rb = GetComponent<Rigidbody2D>();     
 
         _animator = GetComponent<Animator>();
